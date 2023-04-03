2da72e166b86b7303967c7bc6b3215f3b8179884
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-04-03 21:18:04 +0200

    Initial Commit

---
+++ Quoets-Ankara/Quoets_AnkaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quoets_AnkaraApp.swift
+//  Quoets-Ankara
+//
+//  Created by Samet Cagri Aktepe on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Quoets_AnkaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
